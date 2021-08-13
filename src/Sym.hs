65ec0438f671a32e21fb4f01e8e26284555bb158
jamespierog <james.pierog@gmail.com>
2021-08-13 14:57:02 +0800

    added element stuff

--- src/Sym.hs
+++ src/Sym.hs
@@ -34,19 +34,17 @@ x = E [elem1, elem2]
 -- If there is a unique representation that can be obtained by a list of adjecent transpositions then we will use that
 
 -- this defines the binary operation for the symmetric group by using transpositions
-(<->) :: Transposition -> Transposition -> Transposition
-(<->) Id Id = Id
-(<->) (T (a,b)) Id = T (a,b)
-(<->) Id (T (a,b)) = T (a,b)
-(<->) (T (a,b)) (T (c,d)) | T (a,b) == T (c,d) = Id
-                          | (a == c) && (b /= d) = T (d,b)
-                          | (a == d) && (b /= c) = T (b,c)
-                          | (b == c) && (a /= d) = T (d,a)
-                          | (b == d) && (a /= c) = T (a,b)
-                          | (a /= c) && (b /= d) = T (c,d)
+(<->) :: Element -> Element -> Element
+(<->) (E [Id]) (E [Id]) = E [Id]
+(<->) (E [Id]) (E [T (a,b)]) = E [T (a,b)]
+(<->) (E [T (a,b)]) (E [Id]) = E [T (a,b)]
+(<->) (E [T (a,b)]) (E [T (c,d)]) | T (a,b) == T (c,d) = E [Id]
+                                  | a /= c && b /= d = E [T (a,b), T (c,d)]
 
+-- (<->) (E perm1) (E perm2) | => we need an algorithm to simplify down elements to their unique normal form
 
-
-
+-- This function takes an element and simplifies it down to its unique normal form
+-- simplify :: Element -> Element
+-- simplify (E list) =
 
 

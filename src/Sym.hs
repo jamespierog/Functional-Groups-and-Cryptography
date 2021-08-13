8d17e0cb2ae60121bdfa559441cd30593999c9db
jamespierog <james.pierog@gmail.com>
2021-08-13 12:25:04 +0800

    added some stuff

--- src/Sym.hs
+++ src/Sym.hs
@@ -3,7 +3,21 @@ module Sym where
 thing :: IO ()
 thing = putStrLn "thing"
 
--- comp :: (Int, Int) -> (Int, Int) -> (Int, Int)
--- comp (a,b) (c,d) | a ==
+-- A function to define composition
+
+data Transposition = T (Int, Int)
+
+instance Show Transposition where
+  show (T x) = show x
 
 
+{-
+instance Eq Transposition where
+  T (a,b) == T (b,a) = True
+  _       == _       = False
+-}
+
+data Element = Transposition | Id
+
+sym2 :: [Transposition]
+sym2 = [T (1,2), T (1,1)]
